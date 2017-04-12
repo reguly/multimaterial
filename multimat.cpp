635cd9f514420f9a517efa402854c2a91f174cde
Adam Jasz <jasz.adam@hallgato.ppke.hu>
2017-04-12 18:04:39 +0200

    Refactoring

--- multimat.cpp
+++ multimat.cpp
@@ -37,6 +37,192 @@
 #include <cstring>
 #include <cstdio>
 
+void cell_centric_calculations(int sizex, int sizey, int Nmats,
+	double *rho, double *p, double *Vf, double *t,
+	double *V, double *x, double *y,
+	double *n, double *rho_ave)
+{
+	// Cell-centric algorithms
+	// Computational loop 1 - average density in cell
+	for (int j = 0; j < sizey; j++) {
+		for (int i = 0; i < sizex; i++){
+			double ave = 0.0;
+			for (int mat = 0; mat < Nmats; mat++) {
+				// Optimisation:
+				if (Vf[(i+sizex*j)*Nmats+mat] > 0.0)
+					ave += rho[(i+sizex*j)*Nmats+mat]*Vf[(i+sizex*j)*Nmats+mat];
+			}
+			rho_ave[i+sizex*j] = ave/V[i+sizex*j];
+		}
+	}
+
+	// Computational loop 2 - Pressure for each cell and each material
+	for (int j = 0; j < sizey; j++) {
+		for (int i = 0; i < sizex; i++) {
+			for (int mat = 0; mat < Nmats; mat++) {
+				if (Vf[(i+sizex*j)*Nmats+mat] > 0.0) {
+					double nm = n[mat];
+					p[(i+sizex*j)*Nmats+mat] = (nm * rho[(i+sizex*j)*Nmats+mat] * t[(i+sizex*j)*Nmats+mat]) / Vf[(i+sizex*j)*Nmats+mat];
+				}
+				else {
+					p[(i+sizex*j)*Nmats+mat] = 0.0;
+				}
+			}
+		}
+	}
+
+	// Computational loop 3 - Average density of each material over neighborhood of each cell
+	for (int j = 0; j < sizey; j++) {
+		for (int i = 0; i < sizex; i++) {
+			// o: outer
+			double xo = x[i+sizex*j];
+			double yo = y[i+sizex*j];
+
+			// There are at most 9 neighbours in 2D case.
+			// TODO: is this still local to CPU (i.e. no need for memory transfer)?
+			double dsqr[9];
+
+			for (int nj = -1; nj <= 1; nj++) {
+				if ((j + nj < 0) || (j + nj >= sizey)) // TODO: better way?
+					continue;
+
+				for (int ni = -1; nj <= 1; nj++) {
+					if ((i + ni < 0) || (i + ni >= sizex)) // TODO: better way?
+						continue;
+
+					dsqr[(nj+1)*3 + (ni+1)] = 0.0;
+
+					// i: inner
+					double xi = x[(i+ni)+sizex*(j+nj)];
+					double yi = y[(i+ni)+sizex*(j+nj)];
+
+					dsqr[(nj+1)*3 + (ni+1)] += (xo - xi) * (xo - xi);
+					dsqr[(nj+1)*3 + (ni+1)] += (yo - yi) * (yo - yi);
+				}
+			}
+
+			for (int mat = 0; mat < Nmats; mat++) {
+				if (Vf[(i+sizex*j)*Nmats+mat] > 0.0) {
+					double rho_sum = 0.0;
+					int Nn = 0;
+
+					for (int nj = -1; nj <= 1; nj++) {
+						if ((j + nj < 0) || (j + nj >= sizey)) // TODO: better way?
+							continue;
+
+						for (int ni = -1; nj <= 1; nj++) {
+							if ((i + ni < 0) || (i + ni >= sizex)) // TODO: better way?
+								continue;
+
+							if (Vf[((i+ni)+sizex*(j+nj))*Nmats+mat] > 0.0) {
+								rho_sum += rho[((i+ni)+sizex*(j+nj))*Nmats+mat] / dsqr[(nj+1)*3 + (ni+1)];
+								Nn += 1;
+							}
+						}
+					}
+					rho[(i+sizex*j)*Nmats+mat] = rho_sum / Nn;
+				}
+				else {
+					rho[(i+sizex*j)*Nmats+mat] = 0.0;
+				}
+			}
+		}
+	}
+}
+
+void material_centric_calculations(int sizex, int sizey, int Nmats,
+	double *rho, double *p, double *Vf, double *t,
+	double *V, double *x, double *y,
+	double *n, double *rho_ave)
+{
+	int ncells = sizex * sizey;
+
+	// Material-centric algorithms
+	// Computational loop 1 - average density in cell
+	for (int j = 0; j < sizey; j++) {
+		for (int i = 0; i < sizex; i++) {
+			rho_ave[i+sizex*j] = 0.0;
+		}
+	}
+
+	for (int mat = 0; mat < Nmats; mat++) {
+		for (int j = 0; j < sizey; j++) {
+			for (int i = 0; i < sizex; i++) {
+				// Optimisation:
+				if (Vf[ncells*mat + i+sizex*j] > 0.0)
+					rho_ave[i+sizex*j] += rho[ncells*mat + i+sizex*j] * Vf[ncells*mat + i+sizex*j];
+			}
+		}
+	}
+
+	for (int j = 0; j < sizey; j++) {
+		for (int i = 0; i < sizex; i++) {
+			rho_ave[i+sizex*j] /= V[i+sizex*j];
+		}
+	}
+
+	// Computational loop 2 - Pressure for each cell and each material
+	for (int mat = 0; mat < Nmats; mat++) {
+		double nm = n[mat];
+
+		for (int j = 0; j < sizey; j++) {
+			for (int i = 0; i < sizex; i++) {
+				if (Vf[ncells*mat + i+sizex*j] > 0.0) {
+					p[ncells*mat + i+sizex*j] = (nm * rho[ncells*mat + i+sizex*j] * t[ncells*mat + i+sizex*j]) / Vf[ncells*mat + i+sizex*j];
+				}
+				else {
+					p[ncells*mat + i+sizex*j] = 0.0;
+				}
+			}
+		}
+	}
+
+	// Computational loop 3 - Average density of each material over neighborhood of each cell
+	for (int mat = 0; mat < Nmats; mat++) {
+		for (int j = 0; j < sizey; j++) {
+			for (int i = 0; i < sizex; i++) {
+				if (Vf[ncells*mat + i+sizex*j] > 0.0) {
+					// o: outer
+					double xo = x[i+sizex*j];
+					double yo = y[i+sizex*j];
+
+					double rho_sum = 0.0;
+					int Nn = 0;
+
+					for (int nj = -1; nj <= 1; nj++) {
+						if ((j + nj < 0) || (j + nj >= sizey)) // TODO: better way?
+							continue;
+
+						for (int ni = -1; nj <= 1; nj++) {
+							if ((i + ni < 0) || (i + ni >= sizex)) // TODO: better way?
+								continue;
+
+							if (Vf[ncells*mat + (i+ni)+sizex*(j+nj)] > 0.0) {
+								double dsqr = 0.0;
+
+								// i: inner
+								double xi = x[(i+ni)+sizex*(j+nj)];
+								double yi = y[(i+ni)+sizex*(j+nj)];
+
+								dsqr += (xo - xi) * (xo - xi);
+								dsqr += (yo - yi) * (yo - yi);
+
+								rho_sum += rho[ncells*mat + i+sizex*j] / dsqr;
+								Nn += 1;
+							}
+						}
+					}
+
+					rho[ncells*mat + i+sizex*j] = rho_sum / Nn;
+				}
+				else {
+					rho[ncells*mat + i+sizex*j] = 0.0;
+				}
+			}
+		}
+	}
+}
+
 int main(int argc, char* argv[]) {
 	int sizex = 1000;
 	int sizey = 1000;
@@ -283,204 +469,25 @@ int main(int argc, char* argv[]) {
 	printf("Pure cells %d, 2-materials %d, 3 materials %d, 4 materials %d\n",
 		cell_counts_by_mat[0],cell_counts_by_mat[1],cell_counts_by_mat[2],cell_counts_by_mat[3]);
 
-	// Cell-centric algorithms
-	// Computational loop 1 - average density in cell
-	for (int j = 0; j < sizey; j++) {
-		for (int i = 0; i < sizex; i++){
-			double ave = 0.0;
-			for (int mat = 0; mat < Nmats; mat++) {
-				// Optimisation:
-				// if (Vf[(i+sizex*j)*Nmats+mat] > 0.0)
-				ave += rho[(i+sizex*j)*Nmats+mat]*Vf[(i+sizex*j)*Nmats+mat];
-			}
-			rho_ave[i+sizex*j] = ave/V[i+sizex*j];
-		}
-	}
+	cell_centric_calculations(sizex, sizey, Nmats, rho, p, Vf, t, V, x, y, n, rho_ave);
+	material_centric_calculations(sizex, sizey, Nmats, rho_mat, p_mat, Vf_mat, t_mat, V, x, y, n, rho_ave_mat);
 
-	// Computational loop 2 - Pressure for each cell and each material
+	// Check results
 	for (int j = 0; j < sizey; j++) {
 		for (int i = 0; i < sizex; i++) {
-			for (int mat = 0; mat < Nmats; mat++) {
-				if (Vf[(i+sizex*j)*Nmats+mat] > 0.0) {
-					double nm = n[mat];
-					p[(i+sizex*j)*Nmats+mat] = (nm * rho[(i+sizex*j)*Nmats+mat] * t[(i+sizex*j)*Nmats+mat]) / Vf[(i+sizex*j)*Nmats+mat];
-				}
-				else {
-					p[(i+sizex*j)*Nmats+mat] = 0.0;
-				}
-			}
-		}
-	}
-
-	// Computational loop 3 - Average density of each material over neighborhood of each cell
-	for (int j = 0; j < sizey; j++) {
-		for (int i = 0; i < sizex; i++) {
-			// o: outer
-			double xo = x[i+sizex*j];
-			double yo = y[i+sizex*j];
-
-			// There are at most 9 neighbours in 2D case.
-			// TODO: is this still local to CPU (i.e. no need for memory transfer)?
-			double dsqr[9];
-
-			for (int nj = -1; nj <= 1; nj++) {
-				if ((j + nj < 0) || (j + nj >= sizey)) // TODO: better way?
-					continue;
-
-				for (int ni = -1; nj <= 1; nj++) {
-					if ((i + ni < 0) || (i + ni >= sizex)) // TODO: better way?
-						continue;
-
-					dsqr[(nj+1)*3 + (ni+1)] = 0.0;
-
-					// i: inner
-					double xi = x[(i+ni)+sizex*(j+nj)];
-					double yi = y[(i+ni)+sizex*(j+nj)];
-
-					dsqr[(nj+1)*3 + (ni+1)] += (xo - xi) * (xo - xi);
-					dsqr[(nj+1)*3 + (ni+1)] += (yo - yi) * (yo - yi);
-				}
-			}
-
-			for (int mat = 0; mat < Nmats; mat++) {
-				if (Vf[(i+sizex*j)*Nmats+mat] > 0.0) {
-					double rho_sum = 0.0;
-					int Nn = 0;
-
-					for (int nj = -1; nj <= 1; nj++) {
-						if ((j + nj < 0) || (j + nj >= sizey)) // TODO: better way?
-							continue;
-
-						for (int ni = -1; nj <= 1; nj++) {
-							if ((i + ni < 0) || (i + ni >= sizex)) // TODO: better way?
-								continue;
-
-							if (Vf[((i+ni)+sizex*(j+nj))*Nmats+mat] > 0.0) {
-								rho_sum += rho[((i+ni)+sizex*(j+nj))*Nmats+mat] / dsqr[(nj+1)*3 + (ni+1)];
-								Nn += 1;
-							}
-						}
-					}
-					rho[(i+sizex*j)*Nmats+mat] = rho_sum / Nn;
-				}
-				else {
-					rho[(i+sizex*j)*Nmats+mat] = 0.0;
-				}
-			}
-		}
-	}
-
-	// Material-centric algorithms
-	// Computational loop 1 - average density in cell
-	for (int j = 0; j < sizey; j++) {
-		for (int i = 0; i < sizex; i++) {
-			rho_ave_mat[i+sizex*j] = 0.0;
-		}
-	}
-
-	for (int mat = 0; mat < Nmats; mat++) {
-		for (int j = 0; j < sizey; j++) {
-			for (int i = 0; i < sizex; i++) {
-				rho_ave_mat[i+sizex*j] += rho_mat[ncells*mat + i+sizex*j] * Vf_mat[ncells*mat + i+sizex*j];
-			}
-		}
-	}
-
-	for (int j = 0; j < sizey; j++) {
-		for (int i = 0; i < sizex; i++) {
-			rho_ave_mat[i+sizex*j] /= V[i+sizex*j];
-		}
-	}
-
-	// Check result
-	for (int j = 0; j < sizey; j++) {
-		for (int i = 0; i < sizex; i++) {
-			if (abs(rho_ave[i+sizex*j] - rho_ave_mat[i+sizex*j]) > 0.0001) {
+			if (abs(rho_ave[i+sizex*j] - rho_ave[i+sizex*j]) > 0.0001) {
 				printf("1. cell-centric and material-centric values are not equal! (%f, %f, %d, %d)\n",
-					rho_ave[i+sizex*j], rho_ave_mat[i+sizex*j], i, j);
+					rho_ave[i+sizex*j], rho_ave[i+sizex*j], i, j);
 				goto end;
 			}
-		}
-	}	
 
-	// Computational loop 2 - Pressure for each cell and each material
-	for (int mat = 0; mat < Nmats; mat++) {
-		double nm = n[mat];
-
-		for (int j = 0; j < sizey; j++) {
-			for (int i = 0; i < sizex; i++) {
-				if (Vf_mat[ncells*mat + i+sizex*j] > 0.0) {
-					p_mat[ncells*mat + i+sizex*j] = (nm * rho_mat[ncells*mat + i+sizex*j] * t_mat[ncells*mat + i+sizex*j]) / Vf_mat[ncells*mat + i+sizex*j];
-				}
-				else {
-					p_mat[ncells*mat + i+sizex*j] = 0.0;
-				}
-			}
-		}
-	}
-
-	// Check result
-	for (int j = 0; j < sizey; j++) {
-		for (int i = 0; i < sizex; i++) {
 			for (int mat = 0; mat < Nmats; mat++) {
 				if (abs(p[(i+sizex*j)*Nmats+mat] - p_mat[ncells*mat + i+sizex*j]) > 0.0001) {
 					printf("2. cell-centric and material-centric values are not equal! (%f, %f, %d, %d)\n",
 						p[(i+sizex*j)*Nmats+mat], p_mat[ncells*mat + i+sizex*j], i, j);
 					goto end;
 				}
-			}
-		}
-	}
 
-	// Computational loop 3 - Average density of each material over neighborhood of each cell
-	for (int mat = 0; mat < Nmats; mat++) {
-		for (int j = 0; j < sizey; j++) {
-			for (int i = 0; i < sizex; i++) {
-				if (Vf_mat[ncells*mat + i+sizex*j] > 0.0) {
-					// o: outer
-					double xo = x[i+sizex*j];
-					double yo = y[i+sizex*j];
-
-					double rho_sum = 0.0;
-					int Nn = 0;
-
-					for (int nj = -1; nj <= 1; nj++) {
-						if ((j + nj < 0) || (j + nj >= sizey)) // TODO: better way?
-							continue;
-
-						for (int ni = -1; nj <= 1; nj++) {
-							if ((i + ni < 0) || (i + ni >= sizex)) // TODO: better way?
-								continue;
-
-							if (Vf_mat[ncells*mat + (i+ni)+sizex*(j+nj)] > 0.0) {
-								double dsqr = 0.0;
-
-								// i: inner
-								double xi = x[(i+ni)+sizex*(j+nj)];
-								double yi = y[(i+ni)+sizex*(j+nj)];
-
-								dsqr += (xo - xi) * (xo - xi);
-								dsqr += (yo - yi) * (yo - yi);
-
-								rho_sum += rho_mat[ncells*mat + i+sizex*j] / dsqr;
-								Nn += 1;
-							}
-						}
-					}
-
-					rho_mat[ncells*mat + i+sizex*j] = rho_sum / Nn;
-				}
-				else {
-					rho_mat[ncells*mat + i+sizex*j] = 0.0;
-				}
-			}
-		}
-	}
-
-	// Check result
-	for (int j = 0; j < sizey; j++) {
-		for (int i = 0; i < sizex; i++) {
-			for (int mat = 0; mat < Nmats; mat++) {
 				if (abs(rho[(i+sizex*j)*Nmats+mat] - rho_mat[ncells*mat + i+sizex*j]) > 0.0001) {
 					printf("3. cell-centric and material-centric values are not equal! (%f, %f, %d, %d)\n",
 						rho[(i+sizex*j)*Nmats+mat], rho_mat[ncells*mat + i+sizex*j], i, j);
