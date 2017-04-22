53da6093c7874cc08fa515992d47a5affaf32de7
Adam Jasz <jasz.adam@hallgato.ppke.hu>
2017-04-22 11:59:34 +0200

    Rewrite compact algorithms to use neighbors differently

--- compact.cpp
+++ compact.cpp
@@ -58,142 +58,166 @@ void compact_cell_centric(int sizex, int sizey,
 	}
 
 	// Computational loop 3 - Average density of each material over neighborhood of each cell
-	for (int j = 0; j < sizey; j++) {
-		for (int i = 0; i < sizex; i++) {
-			// o: outer
-			double xo = x[i+sizex*j];
-			double yo = y[i+sizex*j];
+#define LOOP_BODY(nj_min, nj_max, ni_min, ni_max) \
+	do { \
+		/* o: outer */ \
+		double xo = x[i+sizex*j]; \
+		double yo = y[i+sizex*j]; \
+		\
+		/* There are at most 9 neighbours in 2D case. */ \
+		double dsqr[9]; \
+		\
+		/* for all neighbours */ \
+		for (int nj = nj_min; nj <= nj_max; nj++) { \
+			for (int ni = ni_min; ni <= ni_max; ni++) { \
+				dsqr[(nj+1)*3 + (ni+1)] = 0.0; \
+				\
+				/* i: inner */ \
+				double xi = x[(i+ni)+sizex*(j+nj)]; \
+				double yi = y[(i+ni)+sizex*(j+nj)]; \
+				\
+				dsqr[(nj+1)*3 + (ni+1)] += (xo - xi) * (xo - xi); \
+				dsqr[(nj+1)*3 + (ni+1)] += (yo - yi) * (yo - yi); \
+			} \
+		} \
+		\
+		int ix = imaterial[i+sizex*j]; \
+		\
+		if (ix <= 0) { \
+			/* condition is 'ix >= 0', this is the equivalent of \
+			 * 'until ix < 0' from the paper */ \
+			for (ix = -ix; ix >= 0; ix = nextfrac[ix]) { \
+				int mat = matids[ix]; \
+				double rho_sum = 0.0; \
+				int Nn = 0; \
+				\
+				/* for all neighbours */ \
+				for (int nj = nj_min; nj <= nj_max; nj++) { \
+					for (int ni = ni_min; ni <= ni_max; ni++) { \
+						int ci = i+ni, cj = j+nj; \
+						int jx = imaterial[ci+sizex*cj]; \
+						\
+						if (jx <= 0) { \
+							/* condition is 'jx >= 0', this is the equivalent of \
+							 * 'until jx < 0' from the paper */ \
+							for (jx = -jx; jx >= 0; jx = nextfrac[jx]) { \
+								if (matids[jx] == mat) { \
+									rho_sum += rho_compact_list[jx] / dsqr[(nj+1)*3 + (ni+1)]; \
+									Nn += 1; \
+									\
+									/* The loop has an extra condition: "and not found". \
+									 * This makes sense, if the material is found, there won't be any more of the same. */ \
+									break; \
+								} \
+							} \
+						} \
+						else { \
+							/* NOTE: In this case, the neighbour is a pure cell, its material index is in jx. \
+							 * In contrast, Algorithm 10 loads matids[jx] which I think is wrong. */ \
+							\
+							/* NOTE: HACK: we index materials from zero, but zero can be a list index */ \
+							int mat_neighbour = jx - 1; \
+							if (mat == mat_neighbour) { \
+								rho_sum += rho_compact[ci+sizex*cj] / dsqr[(nj+1)*3 + (ni+1)]; \
+								Nn += 1; \
+							} \
+						} /* end if (jx <= 0) */ \
+					} /* end for (int ni) */ \
+				} /* end for (int nj) */ \
+				\
+				rho_compact_list[ix] = rho_sum / Nn; \
+			} /* end for (ix = -ix) */ \
+		} /* end if (ix <= 0) */ \
+		else { \
+			/* NOTE: In this case, the cell is a pure cell, its material index is in ix. \
+			 * In contrast, Algorithm 10 loads matids[ix] which I think is wrong. */ \
+			\
+			/* NOTE: HACK: we index materials from zero, but zero can be a list index */ \
+			int mat = ix - 1; \
+			\
+			double rho_sum = 0.0; \
+			int Nn = 0; \
+			\
+			/* for all neighbours */ \
+			for (int nj = nj_min; nj <= nj_max; nj++) { \
+				for (int ni = ni_min; ni <= ni_max; ni++) { \
+					int ci = i+ni, cj = j+nj; \
+					int jx = imaterial[ci+sizex*cj]; \
+					\
+					if (jx <= 0) { \
+						/* condition is 'jx >= 0', this is the equivalent of \
+						 * 'until jx < 0' from the paper */ \
+						for (jx = -jx; jx >= 0; jx = nextfrac[jx]) { \
+							if (matids[jx] == mat) { \
+								rho_sum += rho_compact_list[jx] / dsqr[(nj+1)*3 + (ni+1)]; \
+								Nn += 1; \
+								\
+								/* The loop has an extra condition: "and not found". \
+								 * This makes sense, if the material is found, there won't be any more of the same. */ \
+								break; \
+							} \
+						} \
+					} \
+					else { \
+						/* NOTE: In this case, the neighbour is a pure cell, its material index is in jx. \
+						 * In contrast, Algorithm 10 loads matids[jx] which I think is wrong. */ \
+						\
+						/* NOTE: HACK: we index materials from zero, but zero can be a list index */ \
+						int mat_neighbour = jx - 1; \
+						if (mat == mat_neighbour) { \
+							rho_sum += rho_compact[ci+sizex*cj] / dsqr[(nj+1)*3 + (ni+1)]; \
+							Nn += 1; \
+						} \
+					} /* end if (jx <= 0) */ \
+				} /* end for (int ni) */ \
+			} /* end for (int nj) */ \
+			\
+			rho_compact[i+sizex*j] = rho_sum / Nn; \
+		} /* end else */ \
+	} while (0)
 
-			// There are at most 9 neighbours in 2D case.
-			double dsqr[9];
+	int j, i;
 
-			// for all neighbours
-			for (int nj = -1; nj <= 1; nj++) {
-				if ((j + nj < 0) || (j + nj >= sizey)) // TODO: better way?
-					continue;
+	// j fixed
+	j = 0;
+	for (i = 1; i < sizex - 1; i++) {
+		LOOP_BODY(0, 1, -1, 1);
+	}
+	j = sizey - 1;
+	for (i = 1; i < sizex - 1; i++) {
+		LOOP_BODY(-1, 0, -1, 1);
+	}
 
-				for (int ni = -1; ni <= 1; ni++) {
-					if ((i + ni < 0) || (i + ni >= sizex)) // TODO: better way?
-						continue;
+	// i fixed
+	i = 0;
+	for (j = 1; j < sizey - 1; j++) {
+		LOOP_BODY(-1, 1, 0, 1);
+	}	
+	i = sizex - 1;
+	for (j = 1; j < sizey - 1; j++) {
+		LOOP_BODY(-1, 1, -1, 0);
+	}
 
-					dsqr[(nj+1)*3 + (ni+1)] = 0.0;
+	// corners
+	j = 0; i = 0;
+	LOOP_BODY(0, 1, 0, 1);
 
-					// i: inner
-					double xi = x[(i+ni)+sizex*(j+nj)];
-					double yi = y[(i+ni)+sizex*(j+nj)];
+	j = 0; i = sizex - 1;
+	LOOP_BODY(0, 1, -1, 0);
 
-					dsqr[(nj+1)*3 + (ni+1)] += (xo - xi) * (xo - xi);
-					dsqr[(nj+1)*3 + (ni+1)] += (yo - yi) * (yo - yi);
-				}
-			}
+	j = sizey - 1; i = 0;
+	LOOP_BODY(-1, 0, 0, 1);
 
-			int ix = imaterial[i+sizex*j];
+	j = sizey - 1; i = sizex - 1;
+	LOOP_BODY(-1, 0, -1, 0);
 
-			if (ix <= 0) {
-				// condition is 'ix >= 0', this is the equivalent of
-				// 'until ix < 0' from the paper
-				for (ix = -ix; ix >= 0; ix = nextfrac[ix]) {
-					int mat = matids[ix];
-					double rho_sum = 0.0;
-					int Nn = 0;
-
-					// for all neighbours
-					for (int nj = -1; nj <= 1; nj++) {
-						if ((j + nj < 0) || (j + nj >= sizey)) // TODO: better way?
-							continue;
-
-						for (int ni = -1; ni <= 1; ni++) {
-							if ((i + ni < 0) || (i + ni >= sizex)) // TODO: better way?
-								continue;
-
-							int ci = i+ni, cj = j+nj;
-							int jx = imaterial[ci+sizex*cj];
-
-							if (jx <= 0) {
-								// condition is 'jx >= 0', this is the equivalent of
-								// 'until jx < 0' from the paper
-								for (jx = -jx; jx >= 0; jx = nextfrac[jx]) {
-									if (matids[jx] == mat) {
-										rho_sum += rho_compact_list[jx] / dsqr[(nj+1)*3 + (ni+1)];
-										Nn += 1;
-
-										// The loop has an extra condition: "and not found".
-										// This makes sense, if the material is found, there won't be any more of the same.
-										break;
-									}
-								}
-							}
-							else {
-								// NOTE: In this case, the neighbour is a pure cell, its material index is in jx.
-								// In contrast, Algorithm 10 loads matids[jx] which I think is wrong.
-
-								// NOTE: HACK: we index materials from zero, but zero can be a list index
-								int mat_neighbour = jx - 1;
-								if (mat == mat_neighbour) {
-									rho_sum += rho_compact[ci+sizex*cj] / dsqr[(nj+1)*3 + (ni+1)];
-									Nn += 1;
-								}
-							} // end if (jx <= 0)
-						} // end for (int ni)
-					} // end for (int nj)
-
-					rho_compact_list[ix] = rho_sum / Nn;
-				} // end for (ix = -ix)
-			} // end if (ix <= 0)
-			else {
-				// NOTE: In this case, the cell is a pure cell, its material index is in ix.
-				// In contrast, Algorithm 10 loads matids[ix] which I think is wrong.
-
-				// NOTE: HACK: we index materials from zero, but zero can be a list index
-				int mat = ix - 1;
-
-				double rho_sum = 0.0;
-				int Nn = 0;
-
-				// for all neighbours
-				for (int nj = -1; nj <= 1; nj++) {
-					if ((j + nj < 0) || (j + nj >= sizey)) // TODO: better way?
-						continue;
-
-					for (int ni = -1; ni <= 1; ni++) {
-						if ((i + ni < 0) || (i + ni >= sizex)) // TODO: better way?
-							continue;
-
-						int ci = i+ni, cj = j+nj;
-						int jx = imaterial[ci+sizex*cj];
-
-						if (jx <= 0) {
-							// condition is 'jx >= 0', this is the equivalent of
-							// 'until jx < 0' from the paper
-							for (jx = -jx; jx >= 0; jx = nextfrac[jx]) {
-								if (matids[jx] == mat) {
-									rho_sum += rho_compact_list[jx] / dsqr[(nj+1)*3 + (ni+1)];
-									Nn += 1;
-
-									// The loop has an extra condition: "and not found".
-									// This makes sense, if the material is found, there won't be any more of the same.
-									break;
-								}
-							}
-						}
-						else {
-							// NOTE: In this case, the neighbour is a pure cell, its material index is in jx.
-							// In contrast, Algorithm 10 loads matids[jx] which I think is wrong.
-
-							// NOTE: HACK: we index materials from zero, but zero can be a list index
-							int mat_neighbour = jx - 1;
-							if (mat == mat_neighbour) {
-								rho_sum += rho_compact[ci+sizex*cj] / dsqr[(nj+1)*3 + (ni+1)];
-								Nn += 1;
-							}
-						} // end if (jx <= 0)
-					} // end for (int ni)
-				} // end for (int nj)
-
-				rho_compact[i+sizex*j] = rho_sum / Nn;
-			} // end else
+	for (j = 1; j < sizey - 1; j++) {
+		for (i = 1; i < sizex - 1; i++) {
+			LOOP_BODY(-1, 1, -1, 1);
 		}
 	}
+
+#undef LOOP_BODY
 }
 
 bool compact_check_results(int sizex, int sizey, int Nmats,
